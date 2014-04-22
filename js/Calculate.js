9d5947e6e8d92c0b0ddff19605c9da9b654a6569
heran7 <heran963@gmail.com>
2014-04-23 04:02:43 +0800

    base_v3 setRubric_v2 sampleA_v1  resultG_v1  peerA_v1  history_v1

--- js/Calculate.js
+++ js/Calculate.js
@@ -1,19 +1,24 @@
 var rubric;
 var studentcount = new Array("1","2","3","4","5","6","7","8","9","10");
+var historypa = new Array("第一次","第二次","第三次");
+var thispa = new Array("第一份","第二份","第三份");
 var sample = new Array;
 
 function getRubric () {
-	rubric = document.getElementById("SetR").value;
-	//r = r.replace('\r\n','<br/>');
+	r = document.getElementById("SetR").value;
+	r = r.replace(/\r\n/g,'<br/>');
 	//document.getElementById("SetR").value = r
-	//rubric=r;
+	rubric=r;
 	//alert(rubric);
 	alert("提交成功！");
 }
 
 function showRubric () {
 	x = document.getElementById("showR");  // 找到元素
-    x.innerHTML = rubric;
+	if (document.getElementById("showR").innerText == "")
+	{alert("老師忘記輸入答案了！");}
+	else
+    {x.innerText = rubric;}
 }
 
 function setSample () {	
--- js/leftFrame.js
+++ js/leftFrame.js
@@ -1,4 +1,5 @@
 // JavaScript Documenvt
+var id = 0;
 function changeLeftdiv (divname) {
   	document.getElementById("divDefaulLeft").style.display = "none";
 	document.getElementById("divleftTeacher").style.display = "none";
@@ -13,10 +14,12 @@ function leftDefaul() {//default left frame view
 }
 
 function leftTL(){//after teacher login,left frame view 
+	id = 1;
 	changeLeftdiv ("divleftTeacher");
 }
 
 function leftSL(){//after student login,left frame view
+	id = 0;
 	changeLeftdiv ("divleftStudent");
 }
 
@@ -38,18 +41,16 @@ function help () {
  	changeMaindiv("help");
 }
 
-function mainDefual () {
+function mainDefual () {//main welcome div
 	changeMaindiv("divDefaulMain");
 }
 
-function mainSetR () {
+function mainSetR () {//teacher set rubric div
 	changeMaindiv("divmainSetR");
 }
 
-function mainSampleA () {
-	
+function mainSampleA () {//teacher sample assessment	
 	var s = document.getElementById("sample");
-	
 	for(var i=s.options.length-1; i>=0; i--)
 	{
 		s.options[i].remove();
@@ -59,23 +60,90 @@ function mainSampleA () {
 	{
 		s.add(new Option(sample[i], sample[i]));			
 	}
-	
 	changeMaindiv("divmainSampleA");
+}
+
+function getSampleChange(){//select one sample,show the sample
+	var selected = document.getElementById("sample");
+	if (selected.value == "未選中")
+	{document.getElementById("showSample").style.display = "none";}
+	else
+	{	document.getElementById("showSample").style.display = "";
+		document.getElementById("showSample").innerText = ("第" + selected.value + "名學生的作業為：");}
+
+	var button = document.createElement("input"); //创建一个input对象（提示框按钮）  
+    button.setAttribute("type", "button");
+    button.setAttribute("value", "提交成績");
+	//o.attachEvent("onclick",);        //为控件添加事件
+	document.getElementById("showSample").apppendChild(button);             //添加控件到窗体中
 }
 
 function mainReviewR () {
 	changeMaindiv("divmainReviewR");
 }
 
-function mainResultG () {
+function mainResultG () {//teacher see all students result,student see themself
+	var s = document.getElementById("studentR");
+	for(var i=s.options.length-1; i>=0; i--)
+	{
+		s.options[i].remove();
+	}
+	s.add(new Option("未選中","未選中"));
+	for (var i = 0; i < studentcount.length; i++) 
+	{
+		s.add(new Option(studentcount[i], studentcount[i]));			
+	}
 	changeMaindiv("divmainResultG");
 }
 
-function mainPeerA () {
+function getstudentChange () {
+	
+}
+
+function mainPeerA () {//student peer assessment
+	var s = document.getElementById("historypa");
+	for(var i=s.options.length-1; i>=0; i--)
+	{
+		s.options[i].remove();
+	}
+	s.add(new Option("未選中","未選中"));
+	for (var i = 0; i < historypa.length; i++) 
+	{
+		s.add(new Option(historypa[i], historypa[i]));			
+	}
+	
+	var t = document.getElementById("thispa");
+	for(var i=t.options.length-1; i>=0; i--)
+	{
+		t.options[i].remove();
+	}
+	t.add(new Option("未選中","未選中"));
+	for (var i = 0; i < thispa.length; i++) 
+	{
+		t.add(new Option(thispa[i], thispa[i]));			
+	}
 	changeMaindiv("divmainPeerA");
 }
 
-function mainQueryR () {
+function gethistoryChange(){//select one peer history,show the history
+	var selected = document.getElementById("historypa");
+	if (selected.value == "未選中")
+	{document.getElementById("showHistory").style.display = "none";}
+	else
+	{	document.getElementById("showHistory").style.display = "";
+		document.getElementById("showHistory").innerText = (selected.value + "評量的記錄為：");}
+}
+
+function getThispaChange(){//select this week peer assessment,show the assessment
+	var selected = document.getElementById("thispa");
+	if (selected.value == "未選中")
+	{document.getElementById("showThispa").style.display = "none";}
+	else
+	{	document.getElementById("showThispa").style.display = "";
+		document.getElementById("showThispa").innerText = (selected.value + "待評量的作業為：");}
+}
+
+function mainQueryR () {//student see this week result
 	changeMaindiv("divmainQueryR");
 }
 
