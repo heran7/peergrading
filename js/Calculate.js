1b91d1b8fc0b48ac4a0392b8747c94044d7365ac
heran7 <heran963@gmail.com>
2014-05-05 04:07:12 +0800

    calculate_v6_50% firstassign_v2 setpascore_v1

--- js/Calculate.js
+++ js/Calculate.js
@@ -1,3 +1,14 @@
+function getstudentid (order) {
+	var temp;
+	for (var k=0; k<studentcount; k++){
+		if (order == studentdata[k].studentID)
+		{	
+			temp = k;
+		}
+	}
+	return temp;	
+}
+
 //初始化學生信息
 function initializestudent (studentid,name,ea,interaction){
 	studentdata[studentid-1].studentID = studentid;
@@ -58,24 +69,34 @@ function setSample () {
 
 //第一階段，選擇待評量作業
 function firstassign(studentid) {
-	var temp = studentid;
-	//studnetcount = ea.sort(sortNumber);  //按照ea降序排列后進行分配
+	var temp;
+	var tpa;
+	studentdata.sort(function(a,b) { return a.exattitude < b.exattitude ? 1 : -1;} );  //ea降序排列
+	
+	for (var k=0; k<studentcount; k++){
+		if (studentid == studentdata[k].studentID)
+		{	
+			temp = k;
+			tpa = k+1;
+			//alert(tpa);	
+		}
+	}
 	
 	for (var i=0; i<6; i++){
-		if(temp <= studentcount.length)
+		if(tpa<=(studentcount-1))
 		{
-			thispa[i] = temp+1;
-			//studentdata[temp].assignment[0].pastudent[i] = studentid;
+			studentdata[temp].assignment[0].pastudent[i] = studentdata[tpa].studentID;
+			showtpa[i] = studentdata[tpa].studentID;
 		}
 		else 
 		{
-			temp = temp-studentcount.length;
-			thispa[i] = temp+1;
-			//studentcount[studentid-1][i] = temp+1;
+			tpa = tpa - studentcount;
+			studentdata[temp].assignment[0].pastudent[i] = studentdata[tpa].studentID;
+			showtpa[i] = studentdata[tpa].studentID;
 		}
-		temp += Math.floor(studentcount.length/6);
-		//thispa[i] = studentcount[studentid-1][i];
+		tpa += Math.floor(studentcount/6);
 	}
+	alert(showtpa);
 }
 
 //計算每位同學在討論版中的 互動信息
@@ -86,25 +107,32 @@ function dbinteraction (studentid) {
 	var reply = new Array;
 	var influence = new Array;
 	
-	for (var i=0; i<post; i++)
+	for (var k=0; k<studentcount; k++){
+		if (studentid == studentdata[k].studentID)
+		{	
+			temp = k;
+		}
+	}
+	
+	for (var i=0; i<post.length; i++)
 	{
-		for (var j=0; j<student.length; j++)
+		for (var j=0; j<studentcount; j++)
 		{
-			influence[studentid][j] += 1;
+			studentdata[temp].interaction[studentid] += 1;
 		}
 	}
 	for (var i=0; i<follow.length; i++)
 	{
-		influence[studentid][follow[i]] += 3;
+		//studentdata[temp].interaction[follow[i]的id] += 3;
 	}
 	for (var i=0; i<vote.length; i++)
 	{
-		influence[studentid][vote[i]] += 1;
+		//influence[studentid][vote[i]] += 1;
 	}
 	for (var i=0; i<reply.length; i++)
 	{
-		influence[studentid][reply[i]] += 2;
-		influence[reply[i]][studentid] += 1;
+		//influence[studentid][reply[i]] += 2;
+		//influence[reply[i]][studentid] += 1;
 	}
 	
 }
@@ -203,7 +231,7 @@ function secondassign (studentid) {
 	var highcount = 2;
 	var middlecount = 2;
 	var lowcount = 2;
-	var p = Math.floor(studentcount.length/3);
+	var p = Math.floor(studentcount/3);
 	
 	var selectedcount = 6;  //每份作業只能被評量6次
 	
@@ -243,21 +271,74 @@ function secondassign (studentid) {
 	}
 }
 
+//將評量成績存入數據庫中
+function setpascore () {
+	var ps = document.getElementById("scorelist2").value;
+	var studentid = document.getElementById("thispa").value;
+	var nowlength = 0;
+	//alert(ts + "," + temp);
+	
+	if (ts == "第一份"){
+		var pa1 = showtpa[0];
+		nowlength = studentdata[pa1].assignment[round].pascore.length;
+		studentdata[pa1].assignment[round].pascore[nowlength] = ps;
+	}
+	else if (ts == "第二份") {
+		var pa2 = showtpa[1];
+		nowlength = studentdata[pa2].assignment[round].pascore.length;
+		studentdata[pa2].assignment[round].pascore[nowlength] = ps;
+	}
+	else if (ts == "第三份") {
+		var pa3 = showtpa[2];
+		nowlength = studentdata[pa3].assignment[round].pascore.length;
+		studentdata[pa3].assignment[round].pascore[nowlength] = ps;
+	}
+	else if (ts == "第四份"){
+		var pa4 = showtpa[3];
+		nowlength = studentdata[pa4].assignment[round].pascore.length;
+		studentdata[pa4].assignment[round].pascore[nowlength] = ps;
+	}
+	else if (ts == "第五份") {
+		var pa5 = showtpa[4];
+		nowlength = studentdata[pa5].assignment[round].pascore.length;
+		studentdata[pa5].assignment[round].pascore[nowlength] = ps;
+	}
+	else if (ts == "第六份") {
+		var pa6 = showtpa[5];
+		nowlength = studentdata[pa6].assignment[round].pascore.length;
+		studentdata[pa6].assignment[round].pascore[nowlength] = ps;
+	}else if (ts == "第七份") {
+		var pa1 = showtpa[6];
+		nowlength = studentdata[pa7].assignment[round].pascore.length;
+		studentdata[pa7].assignment[round].pascore[nowlength] = ps;
+	}else{
+		document.getElementById("showThispa").style.display="none";
+	}
+	alert("提交成功！");	
+}
+
 //計算每份作業的成績
-function calculatescore (scorearr) {
-	var s = scorearr;
+function calculatescore (studentid) {
+	var temp;
 	var sum = 0;
 	var pascore = 0;
 	var maxscore = 0;
 	var minscore = 0;
 	
-	for (var i=0; i<s.length; i++)
-	{
-		sum += s[i];
+	for (var k=0; k<studentcount; k++){
+		if (studentid == studentdata[k].studentID)
+		{	
+			temp = k;
+		}
 	}
-	maxscore = get_max_num(s);
-	minscore = get_min_num(s);
-	pascore = (sum - maxscore - minscore)/(s.length - 2);
+	
+	for (var i=0; i<studentdata[temp].assignment[round].pascore.length; i++)
+	{
+		sum += studentdata[temp].assignment[round].pascore[i];
+	}
+	maxscore = get_max_num(studentdata[temp].assignment[round].pascore);
+	minscore = get_min_num(studentdata[temp].assignment[round].pascore);
+	pascore = (sum - maxscore - minscore)/(studentdata[temp].assignment[round].pascore.length - 2);
 	pascore.toFixed(2);  //保留小數點后兩位
 	
 	return pascore;
@@ -287,39 +368,8 @@ function get_min_num(arrs){
 }
 
 
-/*function groupbyinteractive (studentid) {
-
-	var p = Math.floor(studentcount.length/3);
-	interactive.sort(sortNumber);
-	
-}*/
-
 //降序排列
 function sortNumber (a,b) {
 	return b - a;
 	//function(x,y){return x[1]-y[1]};  按數組的第二個元素排序
 }
-
-/*参数定义：
-    datas 要排序的数组，其中每个元素是一个JSON对象{}
-    field 要排序的元素的字段名，将使用该字段进行排序
-    type  排序类型，如果为"down"则为降序排序,否则升序
-*/
-function SortData(datas, field, type) {
-    SortFun.field = field;
-    datas.sort(SortFun);
-    if (type == "down") {
-        datas.reverse();
-    }
-}
-
-function SortFun(data1, data2) {
-    if (data1[SortFun.field] > data2[SortFun.field]) {
-        return 1;
-    }
-    else if (data1[SortFun.field] < data2[SortFun.field]) {
-        return -1;
-    }
-    return 0;
-}
-
--- js/data.js
+++ js/data.js
@@ -9,10 +9,10 @@ var three = new Array("第一份","第二份","第三份");
 var studentresult = new Array("9","8","10","8");
 var historypa = new Array("第一次","第二次","第三次");
 var studentHR = new Array("7","10","8","9");
-//var thispa = new Array("第一份","第二份","第三份","第四份","第五份","第六份");
+var thispa = new Array("第一份","第二份","第三份","第四份","第五份","第六份");
 
 //secondassign test data
-var thispa = new Array;
+var showtpa = new Array;
 var sg = new Array;
 sg[0] = new Array("2","6","7","8","16","17","28");
 sg[1] = new Array("3","4","5","18","19","20","21","22");
